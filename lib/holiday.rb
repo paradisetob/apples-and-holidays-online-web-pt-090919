6815bfe3ced8933d377f7fa6b4fdd6b27820bde6
tracey brimage <onelovenilynn@gmail.com>
2019-10-03 19:48:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,16 +22,17 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+  holiday_hash[winter][christmas][new_years]
+  holiday_supplies[winter][supply]
+
+
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+holiday_hash[spring][memorial_day]
+  holiday_hash[spring][supply]
 
 end
 
