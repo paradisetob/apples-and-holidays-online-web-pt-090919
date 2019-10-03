0559328b88966e8c6902464330863b25cec4c804
tracey brimage <onelovenilynn@gmail.com>
2019-10-03 19:52:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+
   holiday_hash[winter][christmas]
   holiday_supplies[winter][new_years]
-
+binary.pry
 
 
 end
